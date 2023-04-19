2353c49b2a8e52bbdcf981142b1a031ebe4c2d9d
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2023-04-19 16:39:45 +0200

    add readtext function

--- thermostat/src/main/kotlin/it/unibo/ise/lab/thermostat/ReadText.kt
+++ thermostat/src/main/kotlin/it/unibo/ise/lab/thermostat/ReadText.kt
@@ -1,9 +1,8 @@
 package it.unibo.ise.lab.thermostat
 
-import it.unibo.tuprolog.core.Atom
-import it.unibo.tuprolog.core.Substitution
-import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.*
 import it.unibo.tuprolog.core.parsing.TermParser
+import it.unibo.tuprolog.core.parsing.parse
 import it.unibo.tuprolog.solve.ExecutionContext
 import it.unibo.tuprolog.solve.primitive.BinaryRelation
 import it.unibo.tuprolog.solve.primitive.Solve
@@ -13,6 +12,18 @@ import java.io.IOException
 
 object ReadText : BinaryRelation.Functional<ExecutionContext>("read_text") {
     override fun Solve.Request<ExecutionContext>.computeOneSubstitution(first: Term, second: Term): Substitution {
-        TODO()
+        ensuringArgumentIsAtom(0)
+        val path = File((first as Atom).value)
+        if (path.exists() && path.canRead()) {
+            try {
+                val text = path.readText()
+                val value = TermParser.withDefaultOperators().parseTerm(text)
+                return mgu(second, value)
+            } catch (_: Exception) {
+                return Substitution.failed()
+            }
+        } else {
+            return Substitution.failed()
+        }
     }
 }
